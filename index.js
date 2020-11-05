382d966d1a1ea874d071233b8f43e00b7987a11d
Ed Garcia <7903967+edgarciametro@users.noreply.github.com>
2020-11-05 11:17:31 -0500

    Update index.js
    changed all paddings to margins

--- index.js
+++ index.js
@@ -3,43 +3,43 @@ const _ = require('lodash')
 module.exports = function () {
   return function({ addUtilities, e, config }) {
 
-      const paddings = config('theme.paddingSafe.padding',config('theme.padding',{}))
-      const variants = config('variants.paddingSafe',config('variants.padding',{}))
-      const suffix = config('theme.paddingSafe.suffix','safe')
-      const onlySupportsRules = config('theme.paddingSafe.onlySupportsRules',false)
+      const margins = config('theme.marginSafe.margin',config('theme.margin',{}))
+      const variants = config('variants.marginSafe',config('variants.margin',{}))
+      const suffix = config('theme.marginSafe.suffix','safe')
+      const onlySupportsRules = config('theme.marginSafe.onlySupportsRules',false)
 
       const generators = [
         (size, modifier) => (onlySupportsRules ? null : {
-          [`.${e(`p-${modifier}-${suffix}`)}`]: { 'padding': `${size}` },
+          [`.${e(`p-${modifier}-${suffix}`)}`]: { 'margin': `${size}` },
         }),
         (size, modifier) => (onlySupportsRules ? null : {
-          [`.${e(`py-${modifier}-${suffix}`)}`]: { 'padding-top': `${size}`, 'padding-bottom': `${size}` },
-          [`.${e(`px-${modifier}-${suffix}`)}`]: { 'padding-left': `${size}`, 'padding-right': `${size}` },
+          [`.${e(`py-${modifier}-${suffix}`)}`]: { 'margin-top': `${size}`, 'margin-bottom': `${size}` },
+          [`.${e(`px-${modifier}-${suffix}`)}`]: { 'margin-left': `${size}`, 'margin-right': `${size}` },
         }),
         (size, modifier) => (onlySupportsRules ? null : {
-          [`.${e(`pt-${modifier}-${suffix}`)}`]: { 'padding-top': `${size}` },
-          [`.${e(`pr-${modifier}-${suffix}`)}`]: { 'padding-right': `${size}` },
-          [`.${e(`pb-${modifier}-${suffix}`)}`]: { 'padding-bottom': `${size}` },
-          [`.${e(`pl-${modifier}-${suffix}`)}`]: { 'padding-left': `${size}` },
+          [`.${e(`pt-${modifier}-${suffix}`)}`]: { 'margin-top': `${size}` },
+          [`.${e(`pr-${modifier}-${suffix}`)}`]: { 'margin-right': `${size}` },
+          [`.${e(`pb-${modifier}-${suffix}`)}`]: { 'margin-bottom': `${size}` },
+          [`.${e(`pl-${modifier}-${suffix}`)}`]: { 'margin-left': `${size}` },
         }),
 
         (size, modifier) => ({
-          '@supports(padding: max(0px))': {
-            [`.${e(`p-${modifier}-${suffix}`)}`]: { 'padding-top': `max(${size}, env(safe-area-inset-top))`, 'padding-bottom': `max(${size}, env(safe-area-inset-bottom))` , 'padding-left': `max(${size}, env(safe-area-inset-left))` , 'padding-right': `max(${size}, env(safe-area-inset-right))` },
+          '@supports(margin: max(0px))': {
+            [`.${e(`p-${modifier}-${suffix}`)}`]: { 'margin-top': `max(${size}, env(safe-area-inset-top))`, 'margin-bottom': `max(${size}, env(safe-area-inset-bottom))` , 'padding-left': `max(${size}, env(safe-area-inset-left))` , 'padding-right': `max(${size}, env(safe-area-inset-right))` },
 
-            [`.${e(`py-${modifier}-${suffix}`)}`]: { 'padding-top': `max(${size}, env(safe-area-inset-top))`, 'padding-bottom': `max(${size}, env(safe-area-inset-bottom))` },
-            [`.${e(`px-${modifier}-${suffix}`)}`]: { 'padding-left': `max(${size}, env(safe-area-inset-left))`, 'padding-right': `max(${size}, env(safe-area-inset-right))` },
+            [`.${e(`py-${modifier}-${suffix}`)}`]: { 'margin-top': `max(${size}, env(safe-area-inset-top))`, 'margin-bottom': `max(${size}, env(safe-area-inset-bottom))` },
+            [`.${e(`px-${modifier}-${suffix}`)}`]: { 'margin-left': `max(${size}, env(safe-area-inset-left))`, 'margin-right': `max(${size}, env(safe-area-inset-right))` },
 
-            [`.${e(`pt-${modifier}-${suffix}`)}`]: { 'padding-top': `max(${size}, env(safe-area-inset-top))` },
-            [`.${e(`pr-${modifier}-${suffix}`)}`]: { 'padding-right': `max(${size}, env(safe-area-inset-right))` },
-            [`.${e(`pb-${modifier}-${suffix}`)}`]: { 'padding-bottom': `max(${size}, env(safe-area-inset-bottom))` },
-            [`.${e(`pl-${modifier}-${suffix}`)}`]: { 'padding-left': `max(${size}, env(safe-area-inset-left))` },
+            [`.${e(`pt-${modifier}-${suffix}`)}`]: { 'margin-top': `max(${size}, env(safe-area-inset-top))` },
+            [`.${e(`pr-${modifier}-${suffix}`)}`]: { 'margin-right': `max(${size}, env(safe-area-inset-right))` },
+            [`.${e(`pb-${modifier}-${suffix}`)}`]: { 'margin-bottom': `max(${size}, env(safe-area-inset-bottom))` },
+            [`.${e(`pl-${modifier}-${suffix}`)}`]: { 'margin-left': `max(${size}, env(safe-area-inset-left))` },
           }
         }),
       ]
 
       const utilities = _.flatMap(generators, generator => {
-        return _.flatMap(paddings, generator)
+        return _.flatMap(margins, generator)
       })
 
       addUtilities(utilities, variants)
